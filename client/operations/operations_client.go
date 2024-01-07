862709dc6b162cf0dcb4ca27bcecebf55c83c07d
towhid <ktowhid20@gmail.com>
2024-01-08 02:45:04 +0600

    added swagger doc for fetching single database

--- client/operations/operations_client.go
+++ client/operations/operations_client.go
@@ -32,6 +32,8 @@ type ClientOption func(*runtime.ClientOperation)
 type ClientService interface {
 	GetDatabases(params *GetDatabasesParams, opts ...ClientOption) (*GetDatabasesOK, error)
 
+	GetDatabasesID(params *GetDatabasesIDParams, opts ...ClientOption) (*GetDatabasesIDOK, error)
+
 	PostOauthToken(params *PostOauthTokenParams, opts ...ClientOption) (*PostOauthTokenOK, error)
 
 	SetTransport(transport runtime.ClientTransport)
@@ -74,6 +76,46 @@ func (a *Client) GetDatabases(params *GetDatabasesParams, opts ...ClientOption) 
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for GetDatabases: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+GetDatabasesID gets database by ID
+
+Retrieve detailed information about a specific database
+*/
+func (a *Client) GetDatabasesID(params *GetDatabasesIDParams, opts ...ClientOption) (*GetDatabasesIDOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewGetDatabasesIDParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "GetDatabasesID",
+		Method:             "GET",
+		PathPattern:        "/databases/{id}",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &GetDatabasesIDReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*GetDatabasesIDOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for GetDatabasesID: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
