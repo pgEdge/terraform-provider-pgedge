7ae1545b8adb224fe73bfab5d1a9181430cb0dcf
towhid <ktowhid20@gmail.com>
2024-01-08 15:55:27 +0600

    added datanase creation post req

--- client/operations/operations_client.go
+++ client/operations/operations_client.go
@@ -34,6 +34,8 @@ type ClientService interface {
 
 	GetDatabasesID(params *GetDatabasesIDParams, opts ...ClientOption) (*GetDatabasesIDOK, error)
 
+	PostDatabases(params *PostDatabasesParams, opts ...ClientOption) (*PostDatabasesOK, error)
+
 	PostOauthToken(params *PostOauthTokenParams, opts ...ClientOption) (*PostOauthTokenOK, error)
 
 	SetTransport(transport runtime.ClientTransport)
@@ -116,6 +118,46 @@ func (a *Client) GetDatabasesID(params *GetDatabasesIDParams, opts ...ClientOpti
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for GetDatabasesID: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+PostDatabases creates a new database
+
+Create a new database with the specified options
+*/
+func (a *Client) PostDatabases(params *PostDatabasesParams, opts ...ClientOption) (*PostDatabasesOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewPostDatabasesParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "PostDatabases",
+		Method:             "POST",
+		PathPattern:        "/databases",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &PostDatabasesReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*PostDatabasesOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for PostDatabases: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
