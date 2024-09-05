23b3d33926ad1f585ffe91b6a1c3f05244322b2d
KnockOutEZ <ktowhid20@gmail.com>
2024-09-06 04:37:49 +0600

    added tests for data source and resource for cloud account

--- internals/provider/cloud-account/data_source_test.go
+++ internals/provider/cloud-account/data_source_test.go
@@ -1,1 +1,29 @@
-package cloudaccount
+package cloudaccount_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/pgEdge/terraform-provider-pgedge/internals/provider/common"
+)
+
+func TestAccCloudAccountsDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() {},
+		ProtoV6ProviderFactories: common.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: common.ProviderConfig + `
+				data "pgedge_cloud_accounts" "all" {}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify number of cloud accounts returned
+					resource.TestCheckResourceAttr("data.pgedge_cloud_accounts.all", "cloud_accounts.#", "1"),
+					// Verify an attribute of the first cloud account
+					resource.TestCheckResourceAttr("data.pgedge_cloud_accounts.all", "cloud_accounts.0.name", "pgedge"),
+				),
+			},
+		},
+	})
+}
+
