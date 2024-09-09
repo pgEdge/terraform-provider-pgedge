bbdced8585afad856080327756a9f15fd29bacc5
KnockOutEZ <ktowhid20@gmail.com>
2024-09-09 22:44:22 +0600

    added tests for cluster provider

--- internals/provider/cluster/data_source_test.go
+++ internals/provider/cluster/data_source_test.go
@@ -13,11 +13,26 @@ func TestAccClustersDataSource(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: common.ProviderConfig + `
-					data "pgedge_clusters" "tech" {}
-					`,
+					data "pgedge_clusters" "test" {}
+				`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.pgedge_clusters.tech", "clusters.#", "5"),
-					resource.TestCheckResourceAttr("data.pgedge_clusters.tech", "clusters.0.name", "test15"),
+					// Check that we have at least one cluster
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.#"),
+					// Check the first cluster's attributes
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.id"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.name"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.cloud_account_id"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.created_at"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.status"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.regions.#"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.node_location"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.capacity"),
+					// Check for nested attributes
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.firewall_rules.#"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.nodes.#"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.networks.#"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.backup_store_ids.#"),
+					resource.TestCheckResourceAttrSet("data.pgedge_clusters.test", "clusters.0.resource_tags.%"),
 				),
 			},
 		},
